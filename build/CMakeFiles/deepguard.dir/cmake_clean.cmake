e461513a15aec4b7b0130b8986b56a4e2d03557c
Deepesh <161005956+deepesh-kumar-pandey@users.noreply.github.com>
2026-01-21 21:42:28 +0530

    Cmake used to make it universal in windows and linux

---
+++ build/CMakeFiles/deepguard.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepguard.dir/src/Monitor.cpp.obj"
+  "CMakeFiles/deepguard.dir/src/Monitor.cpp.obj.d"
+  "CMakeFiles/deepguard.dir/src/main.cpp.obj"
+  "CMakeFiles/deepguard.dir/src/main.cpp.obj.d"
+  "deepguard.exe"
+  "deepguard.exe.manifest"
+  "deepguard.pdb"
+  "libdeepguard.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deepguard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
